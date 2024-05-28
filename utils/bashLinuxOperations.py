33740d97e772bbe7bb65323e4a92c78f5cc2acb2
Sebastian <ingsebastiantorres95@gmail.com>
2024-05-28 18:19:13 -0500

    Ajuste de escritura y forma bash

--- utils/bashLinuxOperations.py
+++ utils/bashLinuxOperations.py
@@ -2,11 +2,16 @@ import subprocess, sys
 
 command = sys.argv[1:]
 
+
 def execute_shell_to_commit_push_jsonkpifile():
     print("Commiting file to remote repository ")
     try:
-        subprocess.check_output(command, shell = True, executable="/bin/bash", stderr = subprocess.STDOUT)  
+       global result
+       result= subprocess.check_output(command, shell = True, executable="/bin/bash", stderr = subprocess.STDOUT)  
     except subprocess.CalledProcessError as cpe:
-            print("Error executing sh commands", cpe)
+            result = cpe.output
+            #print("Error executing sh commands", cpe)
     finally:
+        for line in result.splitlines():
+            print(line.decode())
         print("Finishing process on remote repository")
